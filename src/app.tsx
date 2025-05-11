19c7da2a82d09be4fa76d8631fd7755422c10f11
Liz Yu <ee07b282@gmail.com>
2025-05-11 06:28:44 +0000

    helloworld

--- src/app.tsx
+++ src/app.tsx
@@ -1,60 +1,246 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
-import { requestOpenExternalUrl } from "@canva/platform";
-import { FormattedMessage, useIntl } from "react-intl";
+import {
+  ArrowUpIcon,
+  Button,
+  FileInput,
+  FileInputItem,
+  Masonry,
+  MasonryItem,
+  ImageCard,
+  Rows,
+  Text,
+  Title,
+  Placeholder,
+} from "@canva/app-ui-kit";
+import { type AppElementOptions, initAppElement } from "@canva/design";
+import { useState, useRef } from "react";
 import * as styles from "styles/components.css";
+import type { QueuedImage } from "@canva/asset";
+import { upload } from "@canva/asset";
+import type { ImageDragConfig } from "@canva/design";
+import { ui } from "@canva/design";
+import type { Image } from "./fake_api";
+import { getImages } from "./fake_api";
+import InfiniteScroll from "react-infinite-scroller";
+import { generatePlaceholders } from "./utils";
 import { useAddElement } from "utils/use_add_element";
+import { useFeatureSupport } from "utils/use_feature_support";
 
-export const DOCS_URL = "https://www.canva.dev/docs/apps/";
+const TARGET_ROW_HEIGHT_PX = 100;
+const NUM_PLACEHOLDERS = 4;
+
+type AppElementData = {
+  imageId: string;
+  width: number;
+  height: number;
+  content: string;
+};
+
+type AppElementChangeEvent = {
+  data: AppElementData;
+  update?: (opts: AppElementOptions<AppElementData>) => Promise<void>;
+};
+
+const uploadImage = async (image: Image): Promise<QueuedImage> => {
+  // This example uses Picsum image URLs, which undergo redirects to the final URL.
+  // Since the `upload` method cannot handle redirect URLs, we fetch the image to resolve the final URL directly.
+  // Skip this step if your API already returns the resolved URL.
+  const { url } = await fetch(image.url);
+
+  const queuedImage = await upload({
+    type: "image",
+    mimeType: "image/jpeg",
+    url,
+    thumbnailUrl: url,
+    width: image.width,
+    height: image.height,
+    aiDisclosure: "none",
+  });
+
+  return queuedImage;
+};
+
+export const Placeholders = generatePlaceholders({
+  numPlaceholders: NUM_PLACEHOLDERS,
+  height: TARGET_ROW_HEIGHT_PX,
+}).map((placeholder, index) => (
+  <MasonryItem
+    targetWidthPx={placeholder.width}
+    targetHeightPx={placeholder.height}
+    key={`placeholder-${index}`}
+  >
+    <Placeholder shape="rectangle" />
+  </MasonryItem>
+));
+
+const initialTemplateState: AppElementChangeEvent = {
+  data: { imageId: "dog",content: "", width: 400, height: 400},
+};
+
+const initialUserImageState: AppElementChangeEvent = {
+  data: { imageId: "",content: "", width: 400, height: 400},
+};
+const initialTemplateUploadState: AppElementChangeEvent = {
+  data: { imageId: "",content: "", width: 400, height: 400},
+};
+
 
 export const App = () => {
-  const addElement = useAddElement();
-  const onClick = () => {
-    addElement({
-      type: "text",
-      children: ["Hello world!"],
-    });
-  };
+  const userImageInputRef = useRef<HTMLInputElement>(null);
+  const templateImageInputRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<Image[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [page, setPage] = useState<number | undefined>(1);
+  const scrollContainerRef = useRef(null);
+  const [templateSelectState, setTemplateSelectState] = useState<AppElementChangeEvent>(initialTemplateState);
+  const {
+    data: { imageId, content, width, height },
+  } = templateSelectState;
+  const [userImageState, setUserImageState] = useState<AppElementChangeEvent>(initialUserImageState);
+  const [templateUploadState, setTemplateUploadState] = useState<AppElementChangeEvent>(initialUserImageState);
+  const [uploading, setUploading] = useState(false);
+  
+  const fetchImages = async () => {
+    if (isFetching || !page) {
+      return;
+    }
 
-  const openExternalUrl = async (url: string) => {
-    const response = await requestOpenExternalUrl({
-      url,
-    });
+    setIsFetching(true);
 
-    if (response.status === "aborted") {
-      // user decided not to navigate to the link
+    try {
+      const { images: newImages, nextPage } = await getImages(page);
+
+      setImages([...images, ...newImages]);
+      setPage(nextPage);
+    } finally {
+      setIsFetching(false);
     }
   };
 
-  const intl = useIntl();
+  const addImageToDesign = async (image: Image) => {
+    
+  };
+
+  const handleFileChange = async (files: File[], onloadend) => {
+    const file = files?.[0]
+    setUploading(true);
+    // Create a FileReader to read the file
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      // Set the image preview URL
+      onloadend(file, reader);
+      setUploading(false)
+    };
+
+    // Read the file as a data URL
+    reader.readAsDataURL(file);
+  }
+
+  const Images = images.map((image, index) => (
+    <MasonryItem
+      targetWidthPx={image.width}
+      targetHeightPx={image.height}
+      key={`MasonryItem-${index}`}
+    >
+      <ImageCard
+        ariaLabel="Select template image"
+        key = {image.title}
+        onClick={() => {
+          setTemplateSelectState((prevState) => {
+            return {
+              ...prevState,
+              data: {
+                ...prevState.data,
+                imageId: image.title,
+              },
+            };
+          });
+        }}
+        thumbnailUrl={image.url}
+        alt={image.title}
+        selectable={true}
+        selected={templateSelectState.data.imageId === image.title}
+      />
+    </MasonryItem>
+  ));
 
   return (
-    <div className={styles.scrollContainer}>
+    <div className={styles.scrollContainer} ref={scrollContainerRef}>
       <Rows spacing="2u">
-        <Text>
-          <FormattedMessage
-            defaultMessage="
-              To make changes to this app, edit the <code>src/app.tsx</code> file,
-              then close and reopen the app in the editor to preview the changes.
-            "
-            description="Instructions for how to make changes to the app. Do not translate <code>src/app.tsx</code>."
-            values={{
-              code: (chunks) => <code>{chunks}</code>,
-            }}
+        <Title size="xsmall">Your image</Title>
+        {userImageState.data.content ? (
+          <Rows spacing="2u">
+            <FileInputItem
+              label={userImageState.data.imageId}
+              onDeleteClick={() => {setUserImageState(initialUserImageState)}}
+            />
+            <ImageCard thumbnailUrl={userImageState.data.content} alt="Preview" />
+          </Rows>
+        ) : (
+          <FileInput
+            multiple={false}
+            onDropAcceptedFiles={(files)=>handleFileChange(files, (file, reader)=>{
+              setUserImageState((prevState) => {
+                return {
+                  ...prevState,
+                  data: {
+                    ...prevState.data,
+                    content: reader.result as string,
+                    imageId: file.name,
+                  },
+                };
+              })
+            })}
+            stretchButton={true}
+            disabled={uploading} 
           />
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          {intl.formatMessage({
-            defaultMessage: "Do something cool",
-            description:
-              "Button text to do something cool. Creates a new text element when pressed.",
-          })}
-        </Button>
-        <Button variant="secondary" onClick={() => openExternalUrl(DOCS_URL)}>
-          {intl.formatMessage({
-            defaultMessage: "Open Canva Apps SDK docs",
-            description:
-              "Button text to open Canva Apps SDK docs. Opens an external URL when pressed.",
-          })}
+        )}
+        <Title size="xsmall">Select template</Title>
+        <InfiniteScroll
+          loadMore={fetchImages}
+          hasMore={page != null}
+          useWindow={false}
+          getScrollParent={() => scrollContainerRef.current}
+        >
+          <Masonry targetRowHeightPx={TARGET_ROW_HEIGHT_PX}>
+            {[...Images, ...(isFetching ? Placeholders : [])]}
+          </Masonry>
+        </InfiniteScroll>
+        <Title size="xsmall">Or upload your template</Title>
+        {templateUploadState.data.content ? (
+          <Rows spacing="2u">
+            <FileInputItem
+              label={templateUploadState.data.imageId}
+              onDeleteClick={() => {setTemplateUploadState(initialTemplateUploadState)}}
+            />
+            <ImageCard thumbnailUrl={templateUploadState.data.content} alt="Preview" />
+          </Rows>
+        ) : (
+          <FileInput
+            multiple={false}
+            onDropAcceptedFiles={(files)=>handleFileChange(files, (file, reader)=>{
+              setTemplateSelectState((prevState) => {
+                return {
+                  ...prevState,
+                  data: {
+                    ...prevState.data,
+                    content: reader.result as string,
+                    imageId: file.name,
+                  },
+                };
+              })
+            })}
+            stretchButton={true}
+            disabled={uploading} 
+          />
+        )}
+        <Button
+          alignment="center"
+          ariaLabel="Label text"
+          onClick={() => {}}
+          variant="primary"
+        >
+          Generate Image
         </Button>
       </Rows>
     </div>
